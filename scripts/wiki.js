2809f7323f243a6b871facebd5248133c0b8f69c
Vitor Lopes <vmnlop@gmail.com>
2020-08-30 17:04:26 +0100

    finish wiki

--- scripts/wiki.js
+++ scripts/wiki.js
@@ -2,21 +2,27 @@ const fetch = require('node-fetch');
 module.exports = function (robot) {
     robot.respond(/search wiki (.*)/i, function (res) {
         searchQuery = res.match[1]
-        endpoint = `https://en.wikipedia.org/w/api.php?origin=*&action=opensearch&srlimit=10&search=${searchQuery}`;
+        endpoint = `http://freshwiki.manjaro.org/w/api.php?origin=*&action=opensearch&srlimit=10&format=json&search=${searchQuery}`;
 
         fetch(endpoint)
             .then(response => response.json())
             .then(data => {
-                titles = data[1]
-                links = data[3]
-                text = ""
-                for (var index in data) {
-                    console.log(index)
-                    
+                console.log(data)
+                results = []
+                for (var title of data[1]) {
+                    value = [title] 
+                    results.push(value)                 
                 }
-                res.reply(text)
+                data[3].forEach(function (value, i) {
+                    results[i].push(value)
+                });
+                output = "I found some results! :robot: <br /> "
+                results.forEach(function (value, i) {
+                    output += `[${value[0]}](${value[1]}) <br /> `
+                });
+                console.log(output)
+                res.reply(output)
             })
             .catch((err) => console.log(err));
-
     })
 }
